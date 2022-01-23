880d4103ff6c4184cb6ad650e07ebded4117d153
alphonso77 <762224+alphonso77@users.noreply.github.com>
2022-01-23 08:22:08 -0500

    test ci cd

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! It\'s 5 o\'clock somewhere!')
+  res.send('Hello World! It\'s 5 o\'clock somewhere! Can I get a witness?')
 })
+app.get('/foo', (req, res) => {
+    res.json({foo: 'bar', bar: 'baz'})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
