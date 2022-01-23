05270f58579ec48157f30d94d0f134d9bc3d80ce
alphonso77 <762224+alphonso77@users.noreply.github.com>
2022-01-23 08:28:12 -0500

    add last update time

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! It\'s 5 o\'clock somewhere! Can I get a witness?')
+  const date = new Date('2022-01-23T08:27:00');
+  res.send(`Hello World! It\'s 5 o\'clock somewhere! Last updated: ${date.toLocaleTimeString()}`)
 })
 app.get('/foo', (req, res) => {
     res.json({foo: 'bar', bar: 'baz'})
